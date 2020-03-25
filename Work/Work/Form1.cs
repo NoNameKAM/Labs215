351060cf02e402b6e18d006defe9a5049b535bfa
NacharovsPadavan <61079090+NacharovsPadavan@users.noreply.github.com>
2020-03-25 22:12:59 +0300

    Add files via upload

---
+++ Work/Work/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Work
+{
+    public partial class Form1 : Form
+    {
+        double buf = 0;
+        string smvl;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label1.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            label1.Text += "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label1.Text += "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            label1.Text = null;
+            buf = 0;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            switch (smvl)
+            {
+                case "+":
+                    {
+                        buf = buf + double.Parse(label1.Text);
+                        label1.Text = Convert.ToString(buf);
+                        buf = 0;
+
+                    }
+                    break;
+                case "-":
+                    {
+                        buf = buf - double.Parse(label1.Text);
+                        label1.Text = Convert.ToString(buf);
+                        buf = 0;
+                    }
+                    break;
+                case "*":
+                    {
+                        buf = buf * double.Parse(label1.Text);
+                        label1.Text = Convert.ToString(buf);
+                        buf = 0;
+                    }
+                    break;
+                case ":":
+                    {
+                        buf = buf / double.Parse(label1.Text);
+                        label1.Text = Convert.ToString(buf);
+                        buf = 0;
+                    }
+                    break;
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            buf += double.Parse(label1.Text);
+            smvl = "+";
+            label1.Text = null;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            buf += double.Parse(label1.Text);
+            smvl = "-";
+            label1.Text = null;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            buf += double.Parse(label1.Text);
+            smvl = "*";
+            label1.Text = null;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            buf += double.Parse(label1.Text);
+            smvl = ":";
+            label1.Text = null;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            label1.Text += ",";
+        }
+    }
+}
